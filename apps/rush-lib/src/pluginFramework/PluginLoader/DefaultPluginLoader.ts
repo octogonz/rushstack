8e8d2654a65be686a5c89bf8e60fbef89bec22b3
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2021-11-10 23:30:23 -0800

    Clarify how PluginLoaderBase.getPackageFolder() is calculated, and eliminate redundant lookups

--- apps/rush-lib/src/pluginFramework/PluginLoader/DefaultPluginLoader.ts
+++ apps/rush-lib/src/pluginFramework/PluginLoader/DefaultPluginLoader.ts
@@ -1,6 +1,16 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
+import { Import } from '@rushstack/node-core-library';
+
 import { PluginLoaderBase } from './PluginLoaderBase';
 
-export class DefaultPluginLoader extends PluginLoaderBase {}
+export class DefaultPluginLoader extends PluginLoaderBase {
+  protected override onGetPackageFolder(): string {
+    const packageFolder: string = Import.resolvePackage({
+      baseFolderPath: __dirname,
+      packageName: this._packageName
+    });
+    return packageFolder;
+  }
+}
